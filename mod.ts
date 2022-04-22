4c05135568afd2de72db84a85661748b6a0ff6d8
Rodney van den Velden <rodneymichael2002@gmail.com>
2022-04-22 12:06:15 +0200

    Send notification when bot comes back online

--- mod.ts
+++ mod.ts
@@ -6,12 +6,26 @@ const client = new CommandClient({
 	owners: config.owners,
 });
 
-client.once('ready', () => {
+client.once('ready', async () => {
 	console.log(`${client.user?.username} is ready!`);
 	client.setPresence({
 		name: 'notifications',
 		type: 'LISTENING',
 	});
+
+	const headers = new Headers({
+		accept: "application/json",
+	});
+
+	headers.append('Title', 'Discord Notification');
+	headers.append('Tags', `gear`);
+	headers.append('Priority', 'low');
+
+	await fetch(`https://${config.ntfyServer}/ntfy-bot`, {
+			method: 'POST',
+			body: `ntfy-bot is back online!`,
+			headers: headers,
+		});
 });
 
 client.commands.loader.loadDirectory('./commands', { maxDepth: 2 });
